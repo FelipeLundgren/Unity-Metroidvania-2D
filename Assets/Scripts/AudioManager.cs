ba8fff32828ffc3523fd026d69d3b751c708119a
felipelundgren-bsatech <felipec5l2@hotmail.com>
2025-11-13 17:49:28 -0300

    feat: adicionando menu

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq;using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public enum SFX
 {
@@ -9,11 +10,19 @@ public enum SFX
     PlayerJump,
     PlayerAttack,
     PlayerHurt,
-    PlayerDeath
+    PlayerDeath,
+    ButtonClick
+    
+}
+public enum MixerGroup
+{
+    Master,
+    SFX,
+    Environment
 }
 
 [Serializable]
-struct SFXConfig
+public struct SFXConfig
 {
     public SFX Type;
     public AudioClip AudioClip;
@@ -22,15 +31,42 @@ struct SFXConfig
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] AudioMixer AudioMixer;
     [SerializeField] private AudioSource SFXAudioSource;
     [SerializeField] private AudioSource EnvironmentAudioSource;
     [SerializeField] private SFXConfig[] SFXConfigs;
 
     private Dictionary<SFX, SFXConfig> SFXs;
+    private Dictionary<MixerGroup, string> MixerGroups;
 
     private void Awake()
     {
+        MixerGroups = new()
+        {
+            { MixerGroup.Master, "MasterVolume" },
+            { MixerGroup.SFX, "SFXVolume" },
+            { MixerGroup.Environment, "EnvironmentVolume" }
+        };
         SFXs = SFXConfigs.ToDictionary(sfxConfig => sfxConfig.Type, sfxConfig => sfxConfig);
+    }
+    public void SetMixerVolume(MixerGroup group, float normalizedValue)
+    {
+        string groupString = MixerGroups[group];
+        float volume = Mathf.Log10(normalizedValue) * 20;
+        AudioMixer.SetFloat(groupString, volume);
+    }
+
+    public float GetMixerVolume(MixerGroup group, bool normalize = true)
+    {
+        string groupString = MixerGroups[group];
+        AudioMixer.GetFloat(groupString, out float volume);
+
+        if (normalize)
+        {
+            volume = Mathf.Pow(10, volume / 20);
+        }
+
+        return volume;
     }
 
     public void PlaySFX(SFX type)
